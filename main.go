b221d31011fb5d56de3bcf3bdd023042d4356564
alexdev404 <alexdev404@users.noreply.github.com>
2025-02-18 10:43:05 -0600

    Add basic type

--- main.go
+++ main.go
@@ -1,20 +1,36 @@
-// FIlename: main.go
+// Filename: main.go
 package main
 
 import (
-	"log"
-	"net/http"
+	"flag"     // Package flag implements command-line flag parsing.
+	"log/slog" // Package log implements a simple logging package.
+	"net/http" // Package http provides HTTP client and server implementations.
+	"os"       // Package os provides a platform-independent interface to operating system functionality.
 )
+
+type config struct {
+	slog.Logger
+}
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from UBIT newsletter"))
 }
 
 func main() {
+	port := flag.String("port", "4000", "Port to run the server on")
+	flag.Parse()
 	// Create a new HTTP server
 	server := http.NewServeMux()
 	server.HandleFunc("/", home)
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", server)
-	log.Fatal(err)
+
+	// Create a new logger
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	// Log the port the server is starting on
+	logger.Info("Starting server on ", "addr", *port)
+
+	err := http.ListenAndServe(":"+*port, server)
+	// Log any errors that occur
+	logger.Error(err.Error())
+	os.Exit(1)
 }
